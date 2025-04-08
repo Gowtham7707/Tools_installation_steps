2ed91e4f9132fe1a2a65af928a6fa5e82c4ec9b3
gowtham <109212247+Gowtham7707@users.noreply.github.com>
2025-04-08 14:20:07 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 
-sudo apt-get update -y
-
-sudo apt-get install curl wget apt-transport-https virtualbox virtualbox-ext-pack -y
-
 echo "1st install docker"
 
-sudo apt update && apt -y install docker.io
+#!/bin/bash
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo chmod 666 /var/run/docker.sock
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#Ref:
+#https://docs.docker.com/engine/install/ubuntu/#installation-methods
 
 echo "Apply updates"
 sudo apt update -y 
@@ -35,4 +45,15 @@ echo "Start the minikube"
 minikube start 
 minikube status
 
+
+##Error Occure means
+Option 1: Use a non-root user (Recommended)
+Create or switch to a normal (non-root) user and then run Minikube:
+
+sudo useradd -m minikubeuser
+sudo usermod -aG docker minikubeuser
+sudo su - minikubeuser
+
+minikube start 
+minikube status
 
